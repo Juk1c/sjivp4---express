d0927f5ee8a0824b9ccc02d4948401b34ec8884d
Karlo B <karlo.banich5kokos@gmailcom>
2023-12-21 10:59:11 +0100

    signup

---
+++ services/auth.js
@@ -0,0 +1,47 @@
+const JWT_SECRET_KEY = process.env.JWT_SECRET_KEY;
+
+const jwt = require("jsonwebtoken");
+
+function getUserJwt(id, email, name, role, expDays = 7) {
+    const tokenData = {
+        sub: id,
+        email: email,
+        name: name,
+        role: role,
+        time: Date.now()
+    };
+
+    const tokenOptions = {
+        expiresIn: expDays * 24 * 60 * 60
+    };
+
+    const token = jwt.sign(tokenData, JWT_SECRET_KEY, tokenOptions);
+
+    return token;
+}
+
+// MIDDLEWARE FOR AUTH COOKIE CHECK
+function checkAuthCookie(req, res, next) {
+    const token = req.cookies["auth"];
+    console.log("COOKIE CHECK", token);
+
+
+    let resolve = null
+    try {
+        result = jwt.verify(token, JWT_SECRET_KEY);
+    } catch (error) {
+        console.log ("ERROR", error);
+        next();
+    }
+
+    req.user = result;
+    next();
+
+
+    // TODO if token verified ok, else eror
+}
+
+module.exports = {
+    getUserJwt,
+    checkAuthCookie
+};
