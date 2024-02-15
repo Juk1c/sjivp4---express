80db3afba08b26b88e06b8166ce9247d3292c0b0
Karlo B <karlo.banich5kokos@gmailcom>
2024-02-15 11:06:05 +0100

    test

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const { engine } = require("express-handlebars");
-const {parseAuthCookie} = require ("./services/auth");
+const { parseAuthCookie } = require("./services/auth");
 
 // ROUTERS IMPORT
 const indexRouter = require('./routes/index');
@@ -15,7 +15,9 @@ const competitonsRouter = require('./routes/competitions');
 const app = express();
 
 // VIEW ENGINE SETUP
-app.engine('handlebars', engine());
+app.engine('handlebars', engine({
+  partialsDir: path.join(__dirname, '/views/partials')
+}));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'handlebars');
 
@@ -43,7 +45,7 @@ app.use(function (err, req, res, next) {
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  if (err.status === 404){
+  if (err.status === 404) {
     res.locals.message = "Tražena stranica ne postoji";
   }
 
